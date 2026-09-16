8efb2825593ad69353414ff31db0d53c979b203e
devv1002 <devmalik1002@gmail.com>
2026-09-16 15:50:29 +0530

    dev

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -40,8 +40,8 @@ function Home() {
                 <h2 className='text-[17px] font-semibold text-slate-100 tracking-tight'>Welcome to CortexAI</h2>
                 <p className='text-[13px] text-slate-500'>Please login to continue using the app.</p>
             </div>
-            <button className='w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-white bg-white hover:bg-gray-200 from-indigo-500 to-violet-700 hover:from-indigo-400 hover:to-violet-600 active:from-indigo-600 active:to-indigo-800 border border-indigo-500/30 shadow-lg shadow-indigo-500/20 hover:shadow-indigo-500/30 transition-all duration-150 cursor-pointer'>
-                <FcGoogle size={15} className='text-white'/>
+            <button className='w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-black/90 bg-white hover:bg-gray-200 transition-all duration-150 cursor-pointer' onClick={googleLogin}>
+                <FcGoogle size={15}/>
                 Continue with Google
             </button>
         </div>
