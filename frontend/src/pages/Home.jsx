ed990edead8b239dd7ec754d93d30abdd8a05060
devv1002 <devmalik1002@gmail.com>
2026-09-17 14:55:49 +0530

    Creating getCurrentUser completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -48,12 +48,12 @@ function Home() {
         <h1>Hello World</h1>
       <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm'>
 
-        <div className='w-[340px] bg-[#13151c] border border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5'>
+        <div className='w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5'>
             <div className='flex flex-col gap-1'>
                 <h2 className='text-[17px] font-semibold text-slate-100 tracking-tight'>Welcome to CortexAI</h2>
                 <p className='text-[13px] text-slate-500'>Please login to continue using the app.</p>
             </div>
-            <button className='w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-black/90 bg-white hover:bg-gray-200 transition-all duration-150 cursor-pointer' onClick={googleLogin}>
+            <button className='w-full flex items-center justify-center gap-3 py-2.75 rounded-xl text-sm font-medium text-black/90 bg-white hover:bg-gray-200 transition-all duration-150 cursor-pointer' onClick={googleLogin}>
                 <FcGoogle size={15}/>
                 Continue with Google
             </button>
