cf9dbb0d4c17b0941057aa569287718762e1715d
devv1002 <devmalik1002@gmail.com>
2026-09-22 12:21:01 +0530

    chatAgent & agent API basic

---
+++ backend/services/agent/routes/agent.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { agent } from "../controllers/agent.controller.js"
+
+const router=express.Router()
+
+router.post("/chat",multer.single("file"),agent)
+
+export default router
