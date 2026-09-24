23ad91f148336bf0d6f3022178bb4abc59d55185
devv1002 <devmalik1002@gmail.com>
2026-09-24 21:41:05 +0530

    part1-done

--- backend/services/agent/config/llmModels.js
+++ backend/services/agent/config/llmModels.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv"
+dotenv.config()
 import { ChatGroq } from "@langchain/groq"
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { ChatOpenRouter } from "@langchain/openrouter";
+
 
 const groq=new ChatGroq({
     model:"openai/gpt-oss-120b"
@@ -9,6 +13,13 @@ const gemini=new ChatGoogleGenerativeAI({
     model:"gemini-2.5-flash"
 })
 
+const openrouter=new ChatOpenRouter({
+    model:"deepseek/deepseek-chat",
+    temperature:0,
+    maxTokens:2500
+})
+
+
 export const getModel=async (agent)=>{
     switch (agent) {
         case "chat":
@@ -16,7 +27,7 @@ export const getModel=async (agent)=>{
         case "search" :    
            return groq;
         case "coding": 
-           return gemini;  
+           return openrouter;  
     
         default:
             return groq;
